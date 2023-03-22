23cb3e1d834e95b839e1f8ce55fc818089bae9e9
MarekKoziak <240933@vutbr.cz>
2023-03-22 12:34:50 +0100

    Added readme with coppied template + added vivado project display_driver

--- 07-display_driver/display_driver/display_driver.srcs/sources_1/new/top.vhd
+++ 07-display_driver/display_driver/display_driver.srcs/sources_1/new/top.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 03/15/2023 12:03:29 PM
+-- Create Date: 03/22/2023 12:12:53 PM
 -- Design Name: 
 -- Module Name: top - Behavioral
 -- Project Name: 
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity top is
     Port ( CLK100MHZ : in STD_LOGIC;
-           SW : in STD_LOGIC;
+           SW : in STD_LOGIC_VECTOR (15 downto 0);
            CA : out STD_LOGIC;
            CB : out STD_LOGIC;
            CC : out STD_LOGIC;
@@ -41,66 +41,66 @@ entity top is
            CE : out STD_LOGIC;
            CF : out STD_LOGIC;
            CG : out STD_LOGIC;
+           DP : out STD_LOGIC;
            AN : out STD_LOGIC_VECTOR (7 downto 0);
            BTNC : in STD_LOGIC);
 end top;
-
+----------------------------------------------------------
+-- Architecture body for top level
+----------------------------------------------------------
 architecture behavioral of top is
-
-  -- 4-bit counter @ 250 ms
-  signal sig_en_250ms : std_logic;                    --! Clock enable signal for Counter0
-  signal sig_cnt_4bit : std_logic_vector(3 downto 0); --! Counter0
-
+  -- No internal signals are needed today:)
 begin
 
   --------------------------------------------------------
-  -- Instance (copy) of clock_enable entity
+  -- Instance (copy) of driver_7seg_4digits entity
   --------------------------------------------------------
-  clk_en0 : entity work.clock_enable
-      generic map(
-          g_MAX => 25000000
-      )
-      port map(
-          clk => CLK100MHZ,
-          rst => BTNC,
-          ce  => sig_en_250ms
-      );
+  driver_seg_4 : entity work.driver_7seg_4digits
+      port map (
+          clk      => CLK100MHZ,
+          rst      => BTNC,
+          data3(3) => SW(15),
+          data3(2) => SW(14),
+          data3(1) => SW(13),
+          data3(0) => SW(12),
+          data2(3) => SW(11),
+          data2(2) => SW(10),
+          data2(1) => SW(9),
+          data2(0) => SW(8),
+          data1(3) => SW(7),
+          data1(2) => SW(6),
+          data1(1) => SW(5),
+          data1(0) => SW(4),
 
-  --------------------------------------------------------
-  -- Instance (copy) of cnt_up_down entity
-  --------------------------------------------------------
-  bin_cnt0 : entity work.cnt_up_down
-     generic map(
-          g_CNT_WIDTH => 4
-      )
-      port map(
-          clk     => CLK100MHZ,
-          rst     => BTNC,
-          en      => sig_en_250ms,
-          cnt_up  => SW,
-          cnt     => sig_cnt_4bit
-      );
+          -- MAP OTHER DATA INPUTS TO ON-BOARD SWITCHES 11:4 HERE
 
-  --------------------------------------------------------
-  -- Instance (copy) of hex_7seg entity
-  --------------------------------------------------------
-  hex2seg : entity work.hex_7seg
-      port map(
-          blank  => BTNC,
-          hex    => sig_cnt_4bit,
+          data0(3) => SW(3),
+          data0(2) => SW(2),
+          data0(1) => SW(1),
+          data0(0) => SW(0),
+
           seg(6) => CA,
           seg(5) => CB,
           seg(4) => CC,
           seg(3) => CD,
           seg(2) => CE,
           seg(1) => CF,
-          seg(0) => CG
+          seg(0) => CG,
+          -- MAP DISPLAY SEGMENTS B:G HERE
+
+
+          -- DECIMAL POINT
+          dp_vect => "0111",
+          dp      => DP,
+
+          -- DIGITS
+          dig(3 downto 0) => AN(3 downto 0)
       );
 
   --------------------------------------------------------
   -- Other settings
   --------------------------------------------------------
-  -- Connect one common anode to 3.3V
-  AN <= b"1111_0111";
+  -- Disconnect the top four digits of the 7-segment display
+  AN(7 downto 4) <= b"1111";
 
 end architecture behavioral;
